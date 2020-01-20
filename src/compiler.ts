e2ae876b29ffc0f562a99da68ebddb90e81173a6
Yegor G <yegorich_555@mail.ru>
2020-01-20 12:28:21 +0300

    Compiler. Fix redundant writing

--- src/compiler.ts
+++ src/compiler.ts
@@ -19,23 +19,6 @@ function reportDiagnostic(diagnostic: ts.Diagnostic): void {
   );
 }
 
-// function hash(str, seed = 0) {
-//   let h1 = 0xdeadbeef ^ seed;
-//   let h2 = 0x41c6ce57 ^ seed;
-//   for (let i = 0, ch; i < str.length; i++) {
-//     ch = str.charCodeAt(i);
-//     h1 = Math.imul(h1 ^ ch, 2654435761);
-//     h2 = Math.imul(h2 ^ ch, 1597334677);
-//   }
-//   h1 =
-//     Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
-//     Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-//   h2 =
-//     Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
-//     Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-//   return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-// }
-
 class VersionContainer {
   major: number;
 
@@ -91,7 +74,6 @@ export default function watchMain(
   };
 
   sysConfig.writeFile = function writeFile(path: string): void {
-    // todo ignore writing by hash
     log.debug("write", path);
     isOnChanged = true;
     // @ts-ignore
@@ -153,11 +135,10 @@ export default function watchMain(
       // todo wait for transpileOnly option: https://github.com/microsoft/TypeScript/issues/29651
     } as ts.CompilerOptions,
     sysConfig,
-    ts.createSemanticDiagnosticsBuilderProgram,
+    ts.createEmitAndSemanticDiagnosticsBuilderProgram,
     reportDiagnostic,
     diagnostic => {
       if (isOnChanged && onChanged && diagnostic.code === 6194) {
-        // todo find a better way
         onChanged(outFile);
         isOnChanged = false;
       } else {
