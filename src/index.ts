4dd52eab27483de709bba0942d8bf3eb858ae3dc
Yegor G <yegorich_555@mail.ru>
2020-02-03 09:21:36 +0300

    Refactoring. Move logic into mockServerMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,10 @@
 import { Application as ExpressApp } from "express";
-import http from "http";
 import log from "./log";
-import NetError from "./netError";
 import MockServerOptions, { defOptions } from "./mockServerOptions";
 import mockServer from "./mockServer";
 import compiler from "./compiler";
 import clearNodeCache from "./clearNodeCache";
-
-let storedPort = 0;
-function addProxyToMockServer(app: ExpressApp): void {
-  let wasError = false;
-
-  // proxy for webpack
-  app.use(function handle(clientReq, clientRes, next) {
-    try {
-      const options = {
-        hostname: "localhost",
-        port: storedPort,
-        path: clientReq.url,
-        method: clientReq.method,
-        headers: clientReq.headers
-      };
-
-      if (
-        clientReq.url === "/" ||
-        clientReq.url.startsWith("/?") ||
-        clientReq.url.startsWith("?") ||
-        !storedPort
-      ) {
-        next();
-        return;
-      }
-
-      const proxy = http.request(options, function callback(res) {
-        wasError = false;
-
-        if (res.statusCode === 404) {
-          next();
-        } else if (res) {
-          clientRes.writeHead(res.statusCode || 200, res.headers);
-          res.pipe(clientRes, {
-            end: true
-          });
-        }
-      });
-
-      clientReq
-        .pipe(proxy, {
-          end: true
-        })
-        .on("error", function onError(ex: NetError) {
-          if (!wasError) {
-            wasError = true;
-            if (ex.code === "ECONNREFUSED") {
-              log.error(
-                `Server is not available on ${ex.address}${
-                  ex.port ? `:${ex.port}` : ""
-                }`
-              );
-            } else {
-              log.error("", ex);
-            }
-          }
-          next();
-        });
-    } catch (ex) {
-      log.error(ex);
-      next();
-    }
-  });
-}
+import mockServerMiddleware from "./mockServerMiddleware";
 
 const webpackMockServer = {
   use(
@@ -85,7 +20,8 @@ const webpackMockServer = {
       };
       log.verbose = opt.verbose;
 
-      addProxyToMockServer(app);
+      mockServerMiddleware(app, 0);
+
       compiler(
         opt.entry,
         opt.tsConfigFileName,
@@ -93,7 +29,7 @@ const webpackMockServer = {
         (outFileNames, outDir) => {
           clearNodeCache(outDir);
           mockServer(outFileNames, opt.port, port => {
-            storedPort = port;
+            mockServerMiddleware(app, port);
           });
         }
       );
---
+++ src/mockServerMiddleware.ts
@@ -0,0 +1,76 @@
+import { Application as ExpressApp } from "express";
+import http from "http";
+import NetError from "./netError";
+import log from "./log";
+
+let storedPort = 0;
+let isDone = false;
+export default function mockServerMiddleware(
+  app: ExpressApp,
+  port: number
+): void {
+  storedPort = port;
+
+  if (isDone) {
+    return;
+  }
+  let wasError = false;
+  isDone = true;
+  app.use(function handle(clientReq, clientRes, next) {
+    try {
+      const options = {
+        hostname: "localhost",
+        port: storedPort,
+        path: clientReq.url,
+        method: clientReq.method,
+        headers: clientReq.headers
+      };
+
+      if (
+        clientReq.url === "/" ||
+        clientReq.url.startsWith("/?") ||
+        clientReq.url.startsWith("?") ||
+        !storedPort
+      ) {
+        next();
+        return;
+      }
+
+      const proxy = http.request(options, function callback(res) {
+        wasError = false;
+
+        if (res.statusCode === 404) {
+          next();
+        } else if (res) {
+          clientRes.writeHead(res.statusCode || 200, res.headers);
+          res.pipe(clientRes, {
+            end: true
+          });
+        }
+      });
+
+      clientReq
+        .pipe(proxy, {
+          end: true
+        })
+        .on("error", function onError(ex: NetError) {
+          if (!wasError) {
+            wasError = true;
+            if (ex.code === "ECONNREFUSED") {
+              log.error(
+                `Server is not available on ${ex.address}${
+                  ex.port ? `:${ex.port}` : ""
+                }`
+              );
+            } else {
+              log.error("", ex);
+            }
+          }
+          next();
+        });
+    } catch (ex) {
+      log.error(ex);
+      next();
+    }
+  });
+}
