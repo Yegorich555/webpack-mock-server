7f64304372402e1cf95c3740380693a132d70af4
Yegor G <yegorich_555@mail.ru>
2020-02-05 14:12:07 +0300

    Webpack. Create test-config

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,16 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line import/no-unresolved
+const path = require("path");
 const webpackMockServer = require("../lib/index.js");
 
 module.exports = {
+  entry: path.resolve(__dirname, "./webpackEntry"),
   stats: { children: false },
   devServer: {
     historyApiFallback: true, // it enables HTML5 mode: https://developer.mozilla.org/en-US/docs/Web/API/History
     stats: {
       children: false // disable console.info for node_modules/*
     },
-    // contentBase: "./public", // folder with static content
-    // watchContentBase: true, // enable hot-reload by changes in contentBase folder
-    before: webpackMockServer.use
+    contentBase: __dirname,
+    publicPath: __dirname,
+    before: app =>
+      webpackMockServer.use(app, {
+        entry: ["test/webpack.mock.ts"],
+        tsConfigFileName: "test/tsconfig.json",
+        verbose: true
+      })
   }
 };
---
+++ test/webpackEntry.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-alert, no-undef
+alert("Webpack is started");
