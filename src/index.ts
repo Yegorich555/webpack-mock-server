15405e9998267b60c0f49144831dd787176340fa
Yahor H <yegorich_555@mail.ru>
2021-06-23 14:15:20 +0300

    Middleware. Fix case for https

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Application as ExpressApp } from "express";
 import http, { Server } from "http";
+import https from "https";
 import { AddressInfo } from "net";
-import log from "./log";
-import MockServerOptions, { defOptions } from "./mockServerOptions";
-import mockServer from "./mockServer";
 import compiler from "./compiler";
-import mockServerMiddleware from "./mockServerMiddleware";
+import log from "./log";
+import mockServer from "./mockServer";
 import mockServerHelper, { MockServerHelper } from "./mockServerHelper";
+import mockServerMiddleware from "./mockServerMiddleware";
+import MockServerOptions, { defOptions } from "./mockServerOptions";
 
 const webpackMockServer = {
   /**
@@ -29,39 +30,56 @@ const webpackMockServer = {
     };
     log.verbose = opt.verbose;
 
+    console.warn(app);
     // important to apply middleware before we made hook otherwise post/put request will be rejected
     mockServerMiddleware(app, opt.port);
 
-    const prev = http.createServer;
-    http.createServer = function hook(): Server {
-      // @ts-ignore
-      const server = prev.apply(this, arguments);
-      server.once("listening", () => {
-        http.createServer = prev; // remove hook because another child-server will be started
-        const address = server.address() as AddressInfo;
-        const parentPort = (address && address.port) || 8080;
-        log.debug("Parent server is started. Starting mock-server...");
-        // set another port because of httpServer in the same process overrides previous listenere
-        if (opt.port === parentPort) {
-          ++opt.port;
-        }
-        try {
-          compiler(
-            opt.entry,
-            opt.tsConfigFileName,
-            opt.compilerOptions,
-            outFileNames => {
-              mockServer(outFileNames, opt, port => {
-                mockServerMiddleware(app, port);
-              });
-            }
-          );
-        } catch (ex) {
-          log.error("Unable to start server\n", ex);
-        }
+    let disposeAll: Array<() => void> = [];
+    function setupHook<T extends Server>(httpOrHttps: {
+      createServer: () => T;
+    }): void {
+      const prev = httpOrHttps.createServer;
+
+      disposeAll.push(() => {
+        // eslint-disable-next-line no-param-reassign
+        httpOrHttps.createServer = prev;
       });
-      return server;
-    };
+
+      // eslint-disable-next-line no-param-reassign
+      httpOrHttps.createServer = function hook(): T {
+        // @ts-ignore
+        const server = prev.apply(this, arguments);
+        server.once("listening", () => {
+          disposeAll.forEach(d => d());
+          disposeAll = [];
+          const address = server.address() as AddressInfo;
+          const parentPort = (address && address.port) || 8080;
+          log.debug("Parent server is started. Starting mock-server...");
+          // set another port because of httpServer in the same process overrides previous listenere
+          if (opt.port === parentPort) {
+            ++opt.port;
+          }
+          try {
+            compiler(
+              opt.entry,
+              opt.tsConfigFileName,
+              opt.compilerOptions,
+              outFileNames => {
+                mockServer(outFileNames, opt, port => {
+                  mockServerMiddleware(app, port);
+                });
+              }
+            );
+          } catch (ex) {
+            log.error("Unable to start server\n", ex);
+          }
+        });
+        return server as T;
+      };
+    }
+
+    setupHook.call(this, http);
+    setupHook.call(this, https);
   },
   /**
    * Add mock functions into webackMockServer
